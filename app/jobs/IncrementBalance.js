be36c80a22d4e181a60b06657534b8031461f403
PierreRAFFA <pierre.raffa@gmail.com>
2017-07-24 20:19:56 +0100

    Send Notifications once the balance updated

--- app/jobs/IncrementBalance.js
+++ app/jobs/IncrementBalance.js
@@ -3,6 +3,8 @@ console.log('incrementBalance');
 console.log(process.env.NODE_ENV);
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const request = require('request');
+const map = require('lodash/map');
 
 // Connection URL
 const url = require('../config')(process.env.NODE_ENV);
@@ -15,10 +17,11 @@ MongoClient.connect(url, (err, db) => {
 
   const userCollection = db.collection('user');
 
+  let ids = [];
   userCollection.find({"balance": { "$lt": 5 }}).toArray((err, users) => {
     assert.equal(err, null);
 
-    console.dir(users);
+    ids = map(users, user => user.firebaseToken);
 
     users.forEach( user => {
       user.balance += 1;
@@ -26,5 +29,33 @@ MongoClient.connect(url, (err, db) => {
     });
 
     db.close();
+
+    sendNotification('balanceUpdated', ids);
   });
 });
+
+/**
+ * Sends the notification by calling the pushNotification service (pushnotificationhost)
+ * @param type May be balanceUpdated
+ * @param ids user firebaseToken list
+ */
+function sendNotification(type, ids) {
+  console.log('Sending notification...');
+  if(ids.length) {
+    const data = {
+      json: true,
+      body: {
+        type: type,
+        ids: ids
+      }
+    };
+
+    request.post('http://pushnotificationhost:3010/api/notifications', data, (error, response) => {
+      if (error){
+        console.log(error);
+      }else{
+        console.log(response.body);
+      }
+    });
+  }
+}
